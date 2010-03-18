ceb70896d7d0cd59e04f8de5b5502d850a51fd6c
geoffrey.roguelon <geoffrey.roguelon@d02e1811-52e6-22e0-6af2-0fe7e4e9e557>
2010-03-18 19:48:09 +0000

    Formatage du code, adaptation du travail réalisé.

--- maze.h
+++ maze.h
@@ -1,18 +1,28 @@
-/* Définit le type d'une case du plateau de jeu */
+// Définition du type tile.
+enum tile_e {
+	EMPTY = 0, 	// Case vide.
+	HARDWALL, 	// Mur indextructible.
+	SOFTWALL, 	// Mur destructible.
+	BOMB,		// Bombe.
+	BONUS, 		// Bonus.
+	EXPLOSION,	// Explosion.
+};
+
+// Définit le type d'une case du plateau de jeu.
 typedef struct {
-  /* A REMPLIR */
+	enum tile_e type; 	// Type de tile.
+	int power;			// Puissance des bombes.
+	int timer;			// Compte à rebour des bombes.
 } tile_t;
 
-
-/* Définit le plateau de jeu */
+// Définit le plateau de jeu.
 typedef struct {
-  int w,h;
-  tile_t *t;
+  int w,h;		// Largeur et Hauteur.
+  tile_t *t;	// Tile.
 } maze_t;
 
-
-/* Déclaration des fonctions concernant le moteur de jeu */
-/* loadMaze: charge d'un fichier un nouveau plateau de jeu */
-maze_t *loadMaze(char *filename);
-/* unloadMaze: désalloue le plateau de jeu */
-void unloadMaze(maze_t *maze);
+// Déclaration des fonctions concernant le moteur de jeu.
+// loadMaze: charge d'un fichier un nouveau plateau de jeu.
+maze_t * loadMaze (char * filename);
+// unloadMaze: désalloue le plateau de jeu.
+void unloadMaze (maze_t * maze);
